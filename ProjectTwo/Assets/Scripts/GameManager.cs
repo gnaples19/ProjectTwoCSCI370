b9f3c2d9e8955f0ebb818be5a9bb3f5f339cf181
Amelia Jones <jonesas@hendrix.edu>
2023-02-13 20:44:16 -0600

    Auto stash before merge of "main" and "origin/main"

--- ProjectTwo/Assets/Scripts/GameManager.cs
+++ ProjectTwo/Assets/Scripts/GameManager.cs
@@ -1,13 +1,42 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance {get; private set;}
+    public GameObject dialogBox;
+    public TextMeshProUGUI dialogText;
+    public void DialogShow(string text){
+    dialogBox.SetActive(true);
+    StopAllCoroutines();
+    StartCoroutine(TypeText(text));
+    }
+
+    public void DialogHide(){
+    dialogBox.SetActive(false);
+    }  
+    
+    IEnumerator TypeText(string text){
+    dialogText.text = "";
+    foreach (char c in text.ToCharArray()){
+        dialogText.text += c;
+        yield return new WaitForSeconds(0.02f);
+    }
+    }
+   
+    void Awake(){
+        if (Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+        Destroy(gameObject);
+    }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
