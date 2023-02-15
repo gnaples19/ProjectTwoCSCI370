ba82f78e7df91dcd5466f6fff9411183ef436231
Amelia Jones <jonesas@hendrix.edu>
2023-02-14 19:25:59 -0600

    Animated parrot and made it flip to face player

--- ProjectTwo/Assets/Scripts/Parrot.cs
+++ ProjectTwo/Assets/Scripts/Parrot.cs
@@ -8,14 +8,28 @@ public class Parrot : MonoBehaviour
     
     public void OnTriggerEnter2D(Collider2D collider2D){
         print("enter");
+        
         if(collider2D.gameObject.CompareTag("Player")){
             GameManager.Instance.DialogShow(text);
+           float playerLoc = collider2D.gameObject.transform.position.x;
+           FlipFace(playerLoc); 
         }
     }
 
     public void OnTriggerExit2D(Collider2D collider2D){
         if(collider2D.gameObject.CompareTag("Player")){
             GameManager.Instance.DialogHide();
+            float playerLoc = collider2D.gameObject.transform.position.x;
+            FlipFace(playerLoc); 
+        }
+    }
+
+    public void FlipFace (float xLoc){
+        if(xLoc > gameObject.transform.position.x){
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(xLoc < gameObject.transform.position.x){
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
     }
     // Start is called before the first frame update
